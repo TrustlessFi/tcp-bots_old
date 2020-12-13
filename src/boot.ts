750d253779838ce18d58e659f87ec83b52ee6265
br4e <68077134+br4e@users.noreply.github.com>
2020-12-13 15:55:44 -0700

    Make auctions bot work better

--- src/boot.ts
+++ src/boot.ts
@@ -3,26 +3,32 @@ import '@nomiclabs/hardhat-ethers';
 import { Auctions } from "../typechain/Auctions";
 import hre from 'hardhat';
 const e = hre.ethers;
+import { BigNumber } from 'ethers';
 
 let timer: NodeJS.Timer | null = null;
+
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 async function main() {
   while(true) {
     console.log("starting loop");
-    console.log("hre", hre, "e", e)
     let AuctionsContract = await e.getContractFactory('Auctions');
-    let auctions = AuctionsContract.attach('0x5eA2921A299f28526F17c27b8D5c64803bc27789') as Auctions;
+    let auctions = AuctionsContract.attach('0x76E49eff4ee5Fa7a5D71F5266BCDDB40eEAf8c4e') as Auctions;
     let shouldStartAuctions = await auctions.shouldStartAuctions();
-    if (shouldStartAuctions.deficitAmount.toNumber() > 0 || shouldStartAuctions.surplusAmount.toNumber() > 0) {
+
+    let zero = BigNumber.from(0);
+
+    if (shouldStartAuctions.deficitAmount.gt(zero) || shouldStartAuctions.surplusAmount.gt(zero)) {
       console.log("should start auctions");
+      await auctions.checkReservesAndStartAuctions();
     } else {
       console.log("should Not start auctions");
     }
-
-    await new Promise(resolve => {
-      timer = setTimeout(resolve, 5);
-      return timer;
-    });
+    await sleep(100);
   }
 }
 
